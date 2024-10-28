0202f8e80cb86ee8515b184d87bb35db2134ce47
Roman <pugachev.ra@yandex.ru>
2024-10-28 17:40:48 +0300

    Updated docker containers' logs accepting process (docker-api gem)

--- timeliner/src/cont_logs.rb
+++ timeliner/src/cont_logs.rb
@@ -1,21 +1,20 @@
 require 'rest-client'
 require 'json'
+require 'docker'
+
+Docker.url = 'unix:///var/run/docker.sock'
 
 def get_container_logs(container_id)
-  docker_sock_url = "http://localhost/containers/#{container_id}/logs?stdout=true&stderr=true"
-
   begin
-    # Sending a request to the Docker API via the Docker socket
-    response = RestClient::Request.execute(
-      method: :get,
-      url: docker_sock_url,
-      headers: { 'Content-Type': 'application/json' },
-      socket: '/var/run/docker.sock' # Connects directly to Docker socket
-    )
-    response.body
-  rescue RestClient::ExceptionWithResponse => e
-    { status: "ERROR", message: "Failed to retrieve logs: #{e.response}" }.to_json
+    container = Docker::Container.get(container_id)
+    logs - container.logs(stditout: true, stderr: true)
+    {status: "SUCCESS", logs: "logs "}.to_json
+  rescue Docker::Error::NotFoundError
+    { status: "ERROR", message: "Container not found" }.to_json
+  rescue Docker::Error::TimeoutError
+    { status: "ERROR", message: "Request to Docker timed out" }.to_json
   rescue StandardError => e
     { status: "ERROR", message: "Unexpected error: #{e.message}" }.to_json
   end
 end
+
